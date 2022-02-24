e9f237d07a04558083354ad30f41c652391e0eac
Moisés <moisesnetored@gmail.com>
2022-02-24 11:38:47 -0400

    feat: Map is rendering on screen with fixed marker

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,40 @@
+
+import { useJsApiLoader, GoogleMap, Marker } from "@react-google-maps/api";
+import { useState } from "react";
+import "./map.css";
+
 export function Home(){
+  const {isLoaded} = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY
+  })
+
+  const position = {
+    lat: -3.088302, lng: -59.983801
+  }
+
+  const [teste, setTeste] = useState(0);
+  //@ts-ignore
+ 
+
   return(
-    <>
-    ola Teste
-    </>
+    <div className="map">
+      {isLoaded ? (
+        <GoogleMap
+          mapContainerStyle={{ width: "100%", height: "100%"}}
+          center={position}
+          zoom={15}
+        >
+          <Marker position={position}  options={{
+            label: {
+              text: "Teste",
+              className: "map-marker"
+            },
+            draggable: true,
+          }}/>
+        </GoogleMap>
+      ): <></>
+      }
+    </div>
   );
 }
