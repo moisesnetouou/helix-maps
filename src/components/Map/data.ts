036cf9109a3e5fd1951be26dad886ab2b8887771
Moisés <moisesnetored@gmail.com>
2022-07-18 17:05:26 -0400

    feat: Defined bookmarks, InfoWindows and functional coordinates.

---
+++ src/components/Map/data.ts
@@ -0,0 +1,30 @@
+export const coordenadas = [
+  {
+    id: 1,
+    name: 'Cidade de Deus - Matriz',
+    lat: -3.0185149,
+    lng: -59.9466991,
+    address: "Av. Nossa Sra. de Fátima - Cidade de Deus 69099-255, Manaus - AM"
+  },
+  {
+    id: 2,
+    name: 'Nova Cidade - Filial',
+    lat: -3.0100918,
+    lng: -59.9712379,
+    address: "Av. Margarita, 2660 - Cidade Nova, 69097-305, Manaus - AM"
+  },
+  {
+    id: 3,
+    name: 'Santa Etelvina',
+    lat: -2.9849032,
+    lng: -60.0094079,
+    address: "Avenida 7 de Maio, Nº 1029 - Santa Etelvina, ao lado do Centro Cultural Rogério Rivoletto, Manaus - AM"
+  },
+  {
+    id: 4,
+    name: 'Amazonino Mendes',
+    lat: -3.0427631,
+    lng: -59.9482286,
+    address: "Rua Itaeté, 24 - Térreo, Amazonino Mendes, Manaus - AM"
+  }
+];
--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,51 +1,73 @@
 
 import { useState } from "react";
-import { useJsApiLoader, GoogleMap, Marker } from "@react-google-maps/api";
+import { useJsApiLoader, GoogleMap, Marker, InfoWindow } from "@react-google-maps/api";
 import Geocode from "react-geocode";
 
 import "./map.scss";
 import loadingIcon from '../../assets/images/loading.svg';
 
-export function Map(){
-  const position = {
-    lat: -3.088302, lng: -59.983801
-  }
+import bigMarkerIcon from '../../assets/images/bigMarker.png';
+import {coordenadas} from './data';
 
-  const [addressData, setAddressData] = useState(null);
-  const [clickedLatLng, setClickedLatLng] = useState(position);
-  const [isClickMap, setIsClickMap] = useState(false);
-  console.log(clickedLatLng)
-  
+interface PositionData {
+  lat: number;
+  lng: number;
+  id: number;
+}
+
+export function Map(){
+  const {isLoaded} = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY,
+  })
+
+  // const position = {
+  //   lat: -3.088302, lng: -59.983801
+  // }
+
+  // const [addressData, setAddressData] = useState(null);
+  // const [clickedLatLng, setClickedLatLng] = useState(position);
+  // const [isClickMap, setIsClickMap] = useState(false);
+
+  const [positionCenter, setPositionCenter] = useState({lat:-3.0115875939510874, lng:  -59.96083744392599})
+
+
+  const [activeMarker, setActiveMarker] = useState(null);
+
   Geocode.setApiKey(process.env.REACT_APP_GOOGLE_KEY);
   Geocode.setLanguage("pt-BR");
 
-  function handleGetAddress(e: any){
-    const coordinates = e.latLng.toJSON()
-    setClickedLatLng(coordinates);
+  // function handleGetAddress(e: any){
+  //   const coordinates = e.latLng.toJSON()
+  //   setClickedLatLng(coordinates);
 
-    const lat = coordinates.lat.toString();
-    const lng = coordinates.lng.toString();
-    console.log('teste', lat)
+  //   const lat = coordinates.lat.toString();
+  //   const lng = coordinates.lng.toString();
+  //   console.log('teste', lat)
     
-    Geocode.fromLatLng(lat, lng).then(
-      (response) => {
-        const address = response.results[0].formatted_address;
-        console.log(response.results)
-        setAddressData(address);
+  //   Geocode.fromLatLng(lat, lng).then(
+  //     (response) => {
+  //       const address = response.results[0].formatted_address;
+  //       console.log(response)
+  //       setAddressData(address);
 
-        console.log(address);
-        setIsClickMap(true);
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
+  //       console.log(address);
+  //       setIsClickMap(true);
+  //     },
+  //     (error) => {
+  //       console.error(error);
+  //     }
+  //   );
+  // }
+
+  function handleCenterMap({lat, lng, id}: PositionData){
+    setPositionCenter({lat, lng});
+
+    if (id === activeMarker) {
+      return;
+    }
+    setActiveMarker(id);
   }
-
-  const {isLoaded} = useJsApiLoader({
-    id: "google-map-script",
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_KEY
-  })
 
   return(
     <main>
@@ -53,37 +75,71 @@ export function Map(){
         {isLoaded ? (
           <GoogleMap
             mapContainerStyle={{ width: "100%", height: "100%"}}
-            center={{lat: clickedLatLng.lat,
-              lng: clickedLatLng.lng,}}
+            center={{
+              lat: positionCenter.lat,
+              lng: positionCenter.lng
+            }}
             zoom={15}
-            onClick={e => handleGetAddress(e)}
+            // onClick={e => handleGetAddress(e)}
           >
-            <Marker position={clickedLatLng}  options={{
-              label: {
-                text: 'Você está aqui!',
-                className: "map-marker"
-              },
-              draggable: false,
-              position: {
-                lat: clickedLatLng.lat,
-                lng: clickedLatLng.lng,
-              }
-            }}/>
+            {coordenadas.map(item => {
+              return(
+                <Marker
+                  key={item.id}
+                  position={{
+                    lat: item.lat,
+                    lng: item.lng
+                  }} 
+                  onClick={()=>handleCenterMap({ lat: item.lat, lng: item.lng,id: item.id})}
+                  options={{
+                    icon: {
+                      url: bigMarkerIcon,
+                      scaledSize: new google.maps.Size(100, 100),
+                      
+                    },
+                    position: {
+                      lat: item.lat,
+                      lng: item.lng,
+                    },
+                  }}
+                >
+                  {activeMarker === item.id ? (
+                    <InfoWindow onCloseClick={() => setActiveMarker(null)} >
+                      <div className="marker-info-box">
+                        <h1>{item.name}</h1>
+                        <span>{item.address}</span>
+                      </div>
+                    </InfoWindow>
+                  ) : null}
+                </Marker>
+              )
+            })}
           </GoogleMap>
         ): (
-          // <div className="loading">
-            <img src={loadingIcon} alt="Carregando" />
-          // </div>
+          <img src={loadingIcon} alt="Carregando" />
         )}
       </div>
 
-      {isClickMap && (
+      {/* {isClickMap && (
         <div className="location">
-        <h3><span>Localização:</span> {addressData}</h3>
+          <h3>
+            <span>Localização:</span> {addressData}
+          </h3>
 
-        <h3><span>Coordenadas:</span> {clickedLatLng.lat}, {clickedLatLng.lng}</h3>
-      </div>
-      )}
+          <h3>
+            <span>Coordenadas:</span> {clickedLatLng.lat}, {clickedLatLng.lng}
+          </h3>
+        </div>
+      )} */}
+
+     <section className="teste-section">
+        {coordenadas.map(select => {
+          return(
+            <button type="button" onClick={()=> handleCenterMap({lat: select.lat, lng: select.lng, id: select.id})}>{select.name}</button>
+          )
+        })}
+     </section>
+
     </main>
   );
 }
